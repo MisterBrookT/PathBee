4cf46b33b612b5185ea1741228eaa02b86fad6a9
MisterBrookT <yinghaotang2001@gmail.com>
2024-08-01 22:27:45 +0800

    experiment: test different gnn model

--- pathbee/gnn/layer.py
+++ pathbee/gnn/layer.py
@@ -26,16 +26,27 @@ class GNN_Layer(Module):
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
+    # baseline
+    # def forward(self, input, adj):
+    #     support = torch.mm(input, self.weight)
+    #     output = torch.spmm(adj, support)
 
-    def forward(self, input, adj):
-        support = torch.mm(input, self.weight)
-        output = torch.spmm(adj, support)
+    #     if self.bias is not None:
+    #         return output + self.bias
+    #     else:
+    #         return output
+
+    # rebuild computation
+    def forward(self, n, adj):
+        output = self.weight
+        for _ in range(n):
+            output = torch.spmm(adj, output)
 
         if self.bias is not None:
             return output + self.bias
         else:
             return output
-
+        
     def __repr__(self):
         return self.__class__.__name__ + ' (' \
                + str(self.in_features) + ' -> ' \
